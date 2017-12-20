d4cb4e88c8583c86cbfadf325a0d0683231ee9fe
charles <charlesemery15@gmail.com>
2017-12-20 13:57:48 +0000

    set min temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,9 +1,26 @@
 describe('A Thermostat', function (){
   var thermostat;
 
-  thermostat = new Thermostat()
+  beforeEach(function(){
+    thermostat = new Thermostat()
+  });
 
   it('should start at 20 degrees', function(){
     expect(thermostat.temperature).toEqual(20);
+  });
+
+  it('should increase the temperature with an up function', function(){
+    thermostat.up()
+    expect(thermostat.temperature).toBeGreaterThan(20)
+  });
+  it('should decrease the temperature with an up function', function(){
+    thermostat.down()
+    expect(thermostat.temperature).toBeLessThan(20)
+  });
+  it('should prevent temp drop lower than 10 degrees', function(){
+    for( var i = 20; i >= 10; i--) {
+      thermostat.down()
+    }
+    expect(function(){thermostat.down()}).toThrowError('Min temp set at 10 degrees')
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,4 +1,14 @@
 function Thermostat(){
-  this.temperature = 20
+  this.min_temp = 10;
+  this.temperature = 20;
 };
- 
+
+Thermostat.prototype.up = function(){
+  return this.temperature ++;
+};
+
+Thermostat.prototype.down = function(){
+  if(this.temperature < this.min_temp ){
+    throw new Error('Min temp set at 10 degrees')};
+  return this.temperature --;
+};
