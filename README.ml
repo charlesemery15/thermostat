369a085cf261890b6b2b481ffe717dbc1daaaf50
charles <31915103+charlesemery15@users.noreply.github.com>
2018-02-19 15:30:28 +0000

    created README.ml
    first draft

---
+++ README.ml
@@ -0,0 +1,7 @@
+#Thermostat
+
+A simple web app that gives the user the ability to change the settings on a thermostat, change the power mode and access to the weather in their location.
+
+##Tech stack
+-Javascript
+-HTML
